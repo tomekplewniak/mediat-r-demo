b1e167208b2bfc629e61bb3ff103180d49983ba5
Tomek Plewniak <tomekplewniak@gmail.com>
2022-03-02 13:49:03 +0100

    Add mocked data access.

---
+++ src/DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using DemoLibrary.Models;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> _people = new();
+
+        public DemoDataAccess()
+        {
+            _people.Add(new PersonModel() { Id = 1, FirstName = "Thomas", LastName = "Plewniak" });
+            _people.Add(new PersonModel() { Id = 1, FirstName = "Sue", LastName = "Storm" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return _people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = _people.Max(_ => _.Id) + 1;
+            _people.Add(p);
+
+            return p;
+        }
+    }
+}
