9cf727a225492c5019932305fc1a0610a52d3bcc
Tomek Plewniak <tomekplewniak@gmail.com>
2022-03-02 16:36:08 +0100

    Add simple command.

--- src/DemoApi/Controllers/PersonController.cs
+++ src/DemoApi/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
-﻿using DemoLibrary.Models;
+﻿using DemoLibrary.Commands;
+using DemoLibrary.Models;
 using DemoLibrary.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -34,8 +35,10 @@ namespace DemoApi.Controllers
 
         // POST api/<PersonController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<PersonModel> Post([FromBody] PersonModel value)
         {
+            var model = new InsertPersonCommand(value.FirstName, value.LastName);
+            return await _mediator.Send(model);
         }
 
         // PUT api/<PersonController>/5
